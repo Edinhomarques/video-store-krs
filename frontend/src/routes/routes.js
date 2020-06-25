20acdb9013e43dbceb23251e3f59111dc042de74
Edson <edson_csl@hotmail.com>
2020-06-24 21:55:30 -0300

    consuming api and passing movie data through ownership

--- frontend/src/routes/routes.js
+++ frontend/src/routes/routes.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import {Switch, Route } from 'react-router-dom';
 
 import MovieList from '../containers/MovieList'
+import api from '../service/api'
 
 export default function Routes() {
+  const [movies, setMovies] = useState([])
+  useEffect( () => {
+    api.get('/movies')
+      .then(response => setMovies([...response.data]))
+  }, [])
   return (
     <Switch>
       <Route path="/" exact>
-        <MovieList />
+        <MovieList movies={movies}/>
       </Route>
     </Switch>
   )
